c6989ea43aede879659b293aec2c2e36ce941c81
YanTree <yantreeland@outlook.com>
2020-11-21 17:14:15 +0800

    add splash image

--- config.el
+++ config.el
@@ -22,28 +22,7 @@
 ;;
 ;;; Font
 (when (display-graphic-p)
-  (defun font-installed-p (font-name)
-    "Check if font with FONT-NAME is available."
-    (find-font (font-spec :name font-name)))
-
-  ;; Set default font
-  (cl-loop for font in '("JetBrains Mono" "Fira Code")
-           when (font-installed-p font)
-           return (set-face-attribute 'default nil
-                                      :font font
-                                      :height (cond (IS-MAC 130)
-                                                    (IS-WINDOWS 100)
-                                                    (t 100))))
-
-  ;; Specify font for all unicode characters
-  (cl-loop for font in '("Symbola" "Symbol")
-           when (font-installed-p font)
-           return(set-fontset-font t 'unicode font nil 'prepend))
-
-  ;; Specify font for Chinese characters
-  (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
-           when (font-installed-p font)
-           return (set-fontset-font t '(#x4e00 . #x9fff) font)))
+  (setq doom-font (font-spec :family "Jetbrains Mono" :size 16)))
 
 ;; Prevents some cases of Emacs flickering
 (add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
@@ -79,6 +58,10 @@
 (after! geiser
   (if IS-WINDOWS
       (setq geiser-chez-binary "C:\\Program Files\\Chez Scheme 9.5.4\\bin\\ta6nt\\scheme")))
+
+;;; :UI doom-dashboard
+;; `https://github.com/tecosaur/emacs-config/blob/master/misc/splash-images'
+(setq fancy-splash-image (concat doom-private-dir "splash.png"))
 
 
 ;;
